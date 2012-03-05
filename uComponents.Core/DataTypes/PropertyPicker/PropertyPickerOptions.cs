5a38fec45afcf192d8fb25367328d9160e373715
leekelleher <leekelleher@gmail.com>
2012-03-05 23:21:59 +0000

    Added new data-type: Property Picker - provides a DropDownList of all the properties from a selected ContentType (e.g. DocumentType, MediaType or MemberType).

---
+++ uComponents.Core/DataTypes/PropertyPicker/PropertyPickerOptions.cs
@@ -0,0 +1,29 @@
+﻿namespace uComponents.Core.DataTypes.PropertyPicker
+{
+	/// <summary>
+	/// Property Picker Options
+	/// </summary>
+	public class PropertyPickerOptions
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="PropertyPickerOptions"/> class.
+		/// </summary>
+		public PropertyPickerOptions()
+		{
+			this.ContentTypeId = string.Empty;
+			this.ObjectTypeId = string.Empty;
+		}
+
+		/// <summary>
+		/// Gets or sets the type of the content.
+		/// </summary>
+		/// <value>The type of the content.</value>
+		public string ContentTypeId { get; set; }
+
+		/// <summary>
+		/// Gets or sets the type of the object.
+		/// </summary>
+		/// <value>The type of the object.</value>
+		public string ObjectTypeId { get; set; }
+	}
+}
