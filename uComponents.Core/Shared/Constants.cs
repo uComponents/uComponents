cf2a0908fb4c997964d86cb190b80b0f5b70ef32
leekelleher <leekelleher@gmail.com>
2012-03-19 14:04:14 +0000

    Reorganised the shared Settings constants into their own class. Extended with well-known Umbraco property aliases.
    --HG--
    branch : 4.0.0
    rename : uComponents.Core/Shared/MediaTypeNames.cs => uComponents.Core/Shared/Constants-MediaTypeNames.cs

---
+++ uComponents.Core/Shared/Constants.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace uComponents.Core.Shared
+{
+	/// <summary>
+	/// Constants class for uComponent specific values.
+	/// </summary>
+	public partial class Constants
+	{
+		/// <summary>
+		/// Name of the application.
+		/// </summary>
+		public const string ApplicationName = "uComponents";
+
+		/// <summary>
+		/// AppSettings key for UI Modules' drag-n-drop.
+		/// </summary>
+		public const string AppKey_DragAndDrop = "ucomponents:DragAndDrop";
+
+		/// <summary>
+		/// AppSettings key for UI Modules' keyboard shortcuts.
+		/// </summary>
+		public const string AppKey_KeyboardShortcuts = "ucomponents:KeyboardShortcuts";
+
+		/// <summary>
+		/// AppSettings key for UI Modules' tray peek.
+		/// </summary>
+		public const string AppKey_TrayPeek = "ucomponents:TrayPeek";
+
+		/// <summary>
+		/// The resource path for the favicon.
+		/// </summary>
+		public const string FaviconResourcePath = "uComponents.Core.Shared.Resources.Images.favicon.ico";
+
+		/// <summary>
+		/// The resource path for the Prevalue Editor stylesheet.
+		/// </summary>
+		public const string PrevalueEditorCssResourcePath = "uComponents.Core.Shared.Resources.Styles.PrevalueEditor.css";
+	}
+}
