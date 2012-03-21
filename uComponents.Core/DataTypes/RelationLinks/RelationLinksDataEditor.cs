ec3078c5ef83373ea84b04258dd788f28eb593b7
Hendy Racher <git@hendyracher.co.uk>
2012-03-21 00:01:56 +0000

    Fleshed out some more of Relation Links (should rename datatype to Links to Related)
    --HG--
    branch : 4.0.0

--- uComponents.Core/DataTypes/RelationLinks/RelationLinksDataEditor.cs
+++ uComponents.Core/DataTypes/RelationLinks/RelationLinksDataEditor.cs
@@ -1,6 +1,12 @@
 ﻿using System.Web.UI;
 using System.Web.UI.WebControls;
 using umbraco.interfaces;
+using System.Web.UI.HtmlControls;
+using umbraco.cms.businesslogic.relation;
+using uComponents.Core.uQueryExtensions;
+using uComponents.Core;
+using umbraco.cms.businesslogic.datatype; // DefaultData
+using umbraco.cms.businesslogic;
 
 namespace uComponents.Core.DataTypes.RelationLinks
 {
@@ -53,10 +59,58 @@ namespace uComponents.Core.DataTypes.RelationLinks
             this.options = options;
         }
 
+        /// <summary>
+        /// Gets the id of the current (content || media || member) node on which this datatype is a property
+        /// </summary>
+        private int CurrentContentId
+        {
+            get
+            {
+                return ((DefaultData)this.data).NodeId;
+            }
+        }
 
         protected override void CreateChildControls()
         {
+            HtmlGenericControl ul = new HtmlGenericControl("ul");
+            ul.Attributes.Add("list-style-type", "none");
+
+            RelationType relationType = new RelationType(this.options.RelationTypeId);
+            if (relationType != null)
+            {                
+                uQuery.UmbracoObjectType parentUmbracoObjectType = relationType.GetParentUmbracoObjectType();
+                uQuery.UmbracoObjectType childUmbracoObjectType = relationType.GetChildUmbracoObjectType();
+               
+                CMSNode relatedCMSNode;
+
+                HtmlGenericControl li;
+                HtmlImage img;
+                HtmlAnchor a;
+
+                foreach (Relation relation in relationType.GetRelations(this.CurrentContentId))
+                {
+                    // this id could be a parent or child (or only the parent if is a one way relation)                                     
+                    if (relation.Parent.Id == this.CurrentContentId)
+                    {
+                        relatedCMSNode = relation.Child;                        
+                    }
+                    else
+                    {
+                        relatedCMSNode = relation.Parent;
+                    }
+
+
+                    li = new HtmlGenericControl("li");
+                    li.InnerText = relatedCMSNode.Text;
+
+                    ul.Controls.Add(li);
+                }
+            }
+
+            this.Controls.Add(ul);
         }
+
+
 
 
         public void Save()
--- uComponents.Core/DataTypes/RelationLinks/RelationLinksOptions.cs
+++ uComponents.Core/DataTypes/RelationLinks/RelationLinksOptions.cs
@@ -1,11 +1,38 @@
-﻿
-namespace uComponents.Core.DataTypes.RelationLinks
-{
+﻿using umbraco.cms.businesslogic.relation;
+using System.Web.Script.Serialization;
 
+namespace uComponents.Core.DataTypes.RelationLinks
+{    
     public class RelationLinksOptions
     {
+
+        /// <summary>
+        /// The Id of the RelationType to use 
+        /// </summary>        
+        public int RelationTypeId { get; set; }
+
+        ///// <summary>
+        ///// Don't serialize this to the persisted settings
+        ///// </summary>
+        //[ScriptIgnore]
+        //public RelationType RelationType
+        //{
+        //    get
+        //    {
+        //        if (this.RelationTypeId > 0)
+        //        {
+        //            return new RelationType(this.RelationTypeId);
+        //        }
+        //        else
+        //        {
+        //            return null;
+        //        }
+        //    }
+        //}
+
         public RelationLinksOptions()
         {
+            this.RelationTypeId = -1;
         }
 
     }
