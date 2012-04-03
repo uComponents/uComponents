3ccde7bc54def9b64aed00260841f66883576451
leekelleher <leekelleher@gmail.com>
2012-04-03 10:29:19 +0100

    uQuery: Added extension method for Member to add/remove a group by name (overloads the AddGroup/RemoveGroup methods)
    --HG--
    branch : 4.0.0

--- uComponents.Core/uQuery/uQueryExtensions/MemberExtensions.cs
+++ uComponents.Core/uQuery/uQueryExtensions/MemberExtensions.cs
@@ -8,6 +8,44 @@ namespace uComponents.Core.uQueryExtensions
 	/// </summary>
 	public static class MemberExtensions
 	{
-		// Methods moved to ContentExtensions.
+		/// <summary>
+		/// Adds a member the group (by group name).
+		/// </summary>
+		/// <param name="member">The member.</param>
+		/// <param name="groupName">Name of the group.</param>
+		public static void AddGroup(this Member member, string groupName)
+		{
+			if (string.IsNullOrWhiteSpace(groupName))
+				return;
+
+			var group = MemberGroup.GetByName(groupName);
+
+			if (group != null)
+			{
+#pragma warning disable 0618
+				member.AddGroup(group.Id);
+#pragma warning restore 0618
+			}
+		}
+
+		/// <summary>
+		/// Removes a member the group (by group name).
+		/// </summary>
+		/// <param name="member">The member.</param>
+		/// <param name="groupName">Name of the group.</param>
+		public static void RemoveGroup(this Member member, string groupName)
+		{
+			if (string.IsNullOrWhiteSpace(groupName))
+				return;
+
+			var group = MemberGroup.GetByName(groupName);
+
+			if (group != null)
+			{
+#pragma warning disable 0618
+				member.RemoveGroup(group.Id);
+#pragma warning restore 0618
+			}
+		}
 	}
 }
