acbc93891cbb80d9c89531d35a037b24034ea79e
leekelleher <leekelleher@gmail.com>
2012-04-22 23:53:39 +0100

    Fixed [workitem:14716] Slider Bug: Able to enter values beyond range. -- Values outside the range will be given the nearest boundary, (lower or upper) -- jQuery numeric limitations on input (allowing commas for ranges)
    --HG--
    branch : 3.0.4

--- uComponents.Core/DataTypes/Slider/SliderDataType.cs
+++ uComponents.Core/DataTypes/Slider/SliderDataType.cs
@@ -125,7 +125,8 @@ namespace uComponents.Core.DataTypes.Slider
 		private void DataEditorControl_OnSave(EventArgs e)
 		{
 			// set the values (on PostBack)
-			double value1, value2 = 0;
+			var value1 = this.m_Control.Options.MinValue;
+			var value2 = this.m_Control.Options.MaxValue;
 			var values = this.m_Control.Text.Split(Settings.COMMA);
 
 			if (double.TryParse(values[0], out value1))
@@ -139,24 +140,23 @@ namespace uComponents.Core.DataTypes.Slider
 			}
 
 			// save the value of the control
-			if (value2 > 0)
+			if (values.Length > 1 && value2 >= this.m_Control.Options.MinValue && value2 <= this.m_Control.Options.MaxValue)
 			{
 				this.Data.Value = string.Concat(value1, Settings.COMMA, value2);
 			}
-			else
+			else if (value1 >= this.m_Control.Options.MinValue && value1 <= this.m_Control.Options.MaxValue)
 			{
+				int value1int;
+
 				// return an integer instead of double if applicable
-				if (this.m_Control.Options.DBType == DBTypes.Integer)
+				if (this.m_Control.Options.DBType == DBTypes.Integer && int.TryParse(value1.ToString(), out value1int))
 				{
-					int value1int = 0;
-					int.TryParse(value1.ToString(), out value1int);
 					this.Data.Value = value1int;
 				}
 				else
 				{
 					this.Data.Value = value1;
 				}
-
 			}
 		}
 	}
