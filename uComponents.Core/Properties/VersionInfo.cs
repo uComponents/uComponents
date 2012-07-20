b723e34a725f78b317e7eb6795a87551c9d1af8e
leekelleher <leekelleher@gmail.com>
2012-07-20 16:01:47 +0100

    Bumped version number for Alpha release.
    --HG--
    branch : 5.0.0-dev

--- uComponents.Core/Properties/VersionInfo.cs
+++ uComponents.Core/Properties/VersionInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("5.0.0")]
-[assembly: AssemblyFileVersion("5.0.0.0")]
-[assembly: AssemblyInformationalVersion("5.0.0-wip")]
+[assembly: AssemblyFileVersion("5.0.752.2")]
+[assembly: AssemblyInformationalVersion("5.0.0-alpha")]
 
 
