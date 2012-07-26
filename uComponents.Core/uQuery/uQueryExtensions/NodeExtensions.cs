ca5dfebba3b3fa03a4e4befbe69d226f700bffa0
leekelleher <leekelleher@gmail.com>
2012-07-26 13:59:56 +0100

    Applied changeset 018bd63444b6 - Random never takes last element
    --HG--
    branch : 3.1.3-dev

--- uComponents.Core/uQuery/uQueryExtensions/NodeExtensions.cs
+++ uComponents.Core/uQuery/uQueryExtensions/NodeExtensions.cs
@@ -506,7 +506,7 @@ namespace uComponents.Core.uQueryExtensions
 		public static Node GetRandom(this List<Node> nodes)
 		{
 			var random = umbraco.library.GetRandom();
-			return nodes[random.Next(0, (nodes.Count - 1))];
+			return nodes[random.Next(0, (nodes.Count))];
 		}
 
 		/// <summary>
