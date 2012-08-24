a6b773525f3ad129fab5ec2513166d2d9d00e419
Hendy Racher <git@hendyracher.co.uk>
2012-08-24 06:55:03 +0100

    fixed guid comment & updated SqlDropDownList to use AbstractOptions
    --HG--
    branch : 4.1.0-dev

--- uComponents.Core/DataTypes/SqlDropDownList/SqlDropDownListOptions.cs
+++ uComponents.Core/DataTypes/SqlDropDownList/SqlDropDownListOptions.cs
@@ -2,23 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.ComponentModel;
+using uComponents.Core.Shared.PrevalueEditors;
 
 namespace uComponents.Core.DataTypes.SqlDropDownList
 {
 	/// <summary>
 	/// 
 	/// </summary>
-	internal class SqlDropDownListOptions
+    internal class SqlDropDownListOptions : AbstractOptions
 	{
 		/// <summary>
 		/// Sql expression used to get drop down list values
 		/// this expression must return both Text and Value fields
 		/// </summary>
+        [DefaultValue("")]
 		public string Sql { get; set; }
 
 		/// <summary>
 		/// Gets or sets an optional connection string (if null then umbraco connection string is used)
 		/// </summary>
+        [DefaultValue("")]
 		public string ConnectionString { get; set; }
 
 		/// <summary>
@@ -26,8 +30,10 @@ namespace uComponents.Core.DataTypes.SqlDropDownList
 		/// </summary>
 		public SqlDropDownListOptions()
 		{
-			this.Sql = string.Empty;
-			this.ConnectionString = null;
 		}
+
+        public SqlDropDownListOptions(bool loadDefaults) : base(loadDefaults)
+        {
+        }
 	}
 }
