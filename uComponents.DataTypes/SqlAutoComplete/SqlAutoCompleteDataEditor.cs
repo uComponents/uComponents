606ec8b6cf7cb6cf70f16cdb7316f3a0ba6cee91
Hendy Racher <git@hendyracher.co.uk>
2012-08-24 07:51:43 +0100

    added skeleton files for a new datatype - Sql AutoComplete
    --HG--
    branch : 5.1.0-dev

---
+++ uComponents.DataTypes/SqlAutoComplete/SqlAutoCompleteDataEditor.cs
@@ -0,0 +1,20 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="SqlAutoCompleteDataEditor.cs" company="Microsoft">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace uComponents.DataTypes.SqlAutoComplete
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class SqlAutoCompleteDataEditor
+    {
+    }
+}
