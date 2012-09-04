487eef6620215b67970d4837c0b309be8d391b42
leekelleher <leekelleher@gmail.com>
2012-09-04 23:23:07 +0100

    Bumped version number for 5.1.0-dev BuildPackage.
    --HG--
    branch : 5.1.0-dev

--- uComponents.Core/Properties/VersionInfo.cs
+++ uComponents.Core/Properties/VersionInfo.cs
@@ -13,8 +13,8 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("5.0.0")]
-[assembly: AssemblyFileVersion("5.0.798.61259")]
-[assembly: AssemblyInformationalVersion("5.0.0-beta")]
+[assembly: AssemblyVersion("5.1.0")]
+[assembly: AssemblyFileVersion("5.1.798.63835")]
+[assembly: AssemblyInformationalVersion("5.1.0-dev")]
 
 
