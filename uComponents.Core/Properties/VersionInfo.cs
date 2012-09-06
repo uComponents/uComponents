af54d63a35e1e091d0dddb32c2c384ba8bb4062b
leekelleher <leekelleher@gmail.com>
2012-09-07 00:47:15 +0100

    BuildPackage: Included .pdb and .xml files in the package build. Assembly version number bumped.
    --HG--
    branch : 5.0.0-dev

--- uComponents.Core/Properties/VersionInfo.cs
+++ uComponents.Core/Properties/VersionInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("5.0.0")]
-[assembly: AssemblyFileVersion("5.0.798.61259")]
+[assembly: AssemblyFileVersion("5.0.801.2099")]
 [assembly: AssemblyInformationalVersion("5.0.0-beta")]
 
 
