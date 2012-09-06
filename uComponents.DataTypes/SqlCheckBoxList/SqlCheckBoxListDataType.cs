7742a987fb0c9894c6066dda08b0c3ce7ac0bc52
leekelleher <leekelleher@gmail.com>
2012-09-07 00:44:46 +0100

    DataTypes: XML doco; house-cleaning & formatting.
    --HG--
    branch : 5.0.0-dev

--- uComponents.DataTypes/SqlCheckBoxList/SqlCheckBoxListDataType.cs
+++ uComponents.DataTypes/SqlCheckBoxList/SqlCheckBoxListDataType.cs
@@ -4,107 +4,129 @@ using umbraco.interfaces;
 
 namespace uComponents.DataTypes.SqlCheckBoxList
 {
-    public class SqlCheckBoxListDataType : umbraco.cms.businesslogic.datatype.BaseDataType, IDataType
-    {
+	/// <summary>
+	/// SQL CheckBoxList data-type
+	/// </summary>
+	public class SqlCheckBoxListDataType : umbraco.cms.businesslogic.datatype.BaseDataType, IDataType
+	{
+		/// <summary>
+		/// The PreValue Editor for the data-type.
+		/// </summary>
+		private SqlCheckBoxListPreValueEditor preValueEditor;
 
-        private SqlCheckBoxListPreValueEditor preValueEditor;
+		/// <summary>
+		/// The Data- Editor for the data-type.
+		/// </summary>
+		private IDataEditor dataEditor;
 
-        private IDataEditor dataEditor;
+		/// <summary>
+		/// The Data object for the data-type.
+		/// </summary>
+		private IData data;
 
-        private IData data;
+		/// <summary>
+		/// 
+		/// </summary>
+		private SqlCheckBoxListOptions options;
 
-        private SqlCheckBoxListOptions options;
+		/// <summary>
+		/// Gets the options.
+		/// </summary>
+		/// <value>The options.</value>
+		private SqlCheckBoxListOptions Options
+		{
+			get
+			{
+				if (this.options == null)
+				{
+					this.options = ((SqlCheckBoxListPreValueEditor)this.PrevalueEditor).Options;
+				}
 
-        private SqlCheckBoxListOptions Options
-        {
-            get
-            {
-                if (this.options == null)
-                {
-                    this.options = ((SqlCheckBoxListPreValueEditor)this.PrevalueEditor).Options;
-                }
+				return this.options;
+			}
+		}
 
-                return this.options;
-            }
-        }
+		/// <summary>
+		/// Gets the name of the data type.
+		/// </summary>
+		/// <value>The name of the data type.</value>
+		public override string DataTypeName
+		{
+			get
+			{
+				return "uComponents: SQL CheckBoxList";
+			}
+		}
 
-        public override string DataTypeName 
-        { 
-            get 
-            { 
-                return "uComponents: SQL CheckBoxList"; 
-            } 
-        }
+		/// <summary>
+		/// Gets the id.
+		/// </summary>
+		/// <value>The id.</value>
+		public override Guid Id
+		{
+			get
+			{
+				return new Guid(DataTypeConstants.SqlCheckBoxListId);
+			}
+		}
 
-        /// <summary>
-        /// Gets the id.
-        /// </summary>
-        /// <value>The id.</value>
-        public override Guid Id
-        {
-            get
-            {
-                return new Guid(DataTypeConstants.SqlCheckBoxListId);
-            }
-        }
+		/// <summary>
+		/// Lazy load the associated PreValueEditor instance, 
+		/// this is constructed supplying 'this'
+		/// </summary>
+		public override IDataPrevalue PrevalueEditor
+		{
+			get
+			{
+				if (this.preValueEditor == null)
+				{
+					this.preValueEditor = new SqlCheckBoxListPreValueEditor(this);
+				}
 
-        /// <summary>
-        /// Lazy load the associated PreValueEditor instance, 
-        /// this is constructed supplying 'this'
-        /// </summary>
-        public override IDataPrevalue PrevalueEditor
-        {
-            get
-            {
-                if (this.preValueEditor == null)
-                {
-                    this.preValueEditor = new SqlCheckBoxListPreValueEditor(this);
-                }
+				return this.preValueEditor;
+			}
+		}
 
-                return this.preValueEditor;
-            }
-        }
+		/// <summary>
+		/// Lazy load the assocated DataEditor, 
+		/// this is constructed supplying the data value stored by the PreValueEditor, and also the configuration settings of the PreValueEditor 
+		/// </summary>
+		public override IDataEditor DataEditor
+		{
+			get
+			{
+				if (this.dataEditor == null)
+				{
+					this.dataEditor = new SqlCheckBoxListDataEditor(this.Data, this.Options);
+				}
 
-        /// <summary>
-        /// Lazy load the assocated DataEditor, 
-        /// this is constructed supplying the data value stored by the PreValueEditor, and also the configuration settings of the PreValueEditor 
-        /// </summary>
-        public override IDataEditor DataEditor
-        {
-            get
-            {
-                if (this.dataEditor == null)
-                {
-                    this.dataEditor = new SqlCheckBoxListDataEditor(this.Data, this.Options);
-                }
+				return this.dataEditor;
+			}
+		}
 
-                return this.dataEditor;
-            }
-        }
+		/// <summary>
+		/// Lazy load an empty DefaultData object, this is used to pass data between the PreValueEditor and the DataEditor
+		/// </summary>
+		public override IData Data
+		{
+			get
+			{
+				if (this.data == null)
+				{
+					if (this.Options.UseXml)
+					{
+						// Storing an Xml fragment
+						this.data = new XmlData(this);
+					}
+					else
+					{
+						// Storing a Csv
+						this.data = new umbraco.cms.businesslogic.datatype.DefaultData(this);
+					}
+				}
 
-        /// <summary>
-        /// Lazy load an empty DefaultData object, this is used to pass data between the PreValueEditor and the DataEditor
-        /// </summary>
-        public override IData Data
-        {
-            get
-            {
-                if (this.data == null)
-                {
-                    if (this.Options.UseXml)
-                    {
-                        // Storing an Xml fragment
-                        this.data = new XmlData(this);
-                    }
-                    else
-                    {
-                        // Storing a Csv
-                        this.data = new umbraco.cms.businesslogic.datatype.DefaultData(this);
-                    }
-                }
-
-                return this.data;
-            }
-        }
-    }
+				return this.data;
+			}
+		}
+	}
 }
