fe7aeefad0b6fdd9fac1cb0c31f02cf7080d0e6c
leekelleher <leekelleher@gmail.com>
2012-09-07 01:43:30 +0100

    committed stub file
    --HG--
    branch : 5.1.0-dev

---
+++ uComponents.Core/Modules/uComponentsModule.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Web;
+
+namespace uComponents.Core.Modules
+{
+	public class uComponentsModule : IHttpModule
+	{
+		public void Dispose() { }
+		public void Init(HttpApplication context) { }
+	}
+}
