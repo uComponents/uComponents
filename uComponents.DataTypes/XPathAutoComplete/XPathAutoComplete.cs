cd472545de47a1c2acb7f5c4ed6f990a5be3f6c2
Hendy Racher <git@hendyracher.co.uk>
2012-09-27 22:07:36 +0100

    XPath AC - SQL AC: uQuery.IGetProperty return types (not in solution)
    --HG--
    branch : 5.1.0-dev

---
+++ uComponents.DataTypes/XPathAutoComplete/XPathAutoComplete.cs
@@ -0,0 +1,42 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using umbraco.NodeFactory;
+using umbraco;
+using umbraco.cms.businesslogic.media;
+using umbraco.cms.businesslogic.member;
+using System.Xml;
+
+namespace uComponents.DataTypes.XPathAutoComplete
+{
+    public class XPathAutoComplete : uQuery.IGetProperty
+    {
+        public XPathAutoComplete()
+        {
+
+        }
+
+        public IEnumerable<Node> SelectedNodes
+        {
+            get;
+            private set;
+        }
+
+        public IEnumerable<Media> SelectedMedia            
+        {
+            get;
+            private set;
+        }
+
+        public IEnumerable<Member> SelectedMembers
+        {
+            get;
+            private set;
+        }
+
+        uQuery.IGetProperty.LoadPropertyValue(string value)
+        {
+            
+        }
+    }
+}
