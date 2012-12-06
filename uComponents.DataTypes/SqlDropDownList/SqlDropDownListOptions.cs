46d9cc440cda2469e73770a6b6e4ca94f49432e1
leekelleher <leekelleher@gmail.com>
2012-12-06 16:06:26 +0000

    Refactored SQL data-types to use Umbraco's SqlHelper. Now compatiable with SQLCE and MySQL. Abstracted out the SQL options, for consistence.
    --HG--
    branch : 5.3.0-dev

--- uComponents.DataTypes/SqlDropDownList/SqlDropDownListOptions.cs
+++ uComponents.DataTypes/SqlDropDownList/SqlDropDownListOptions.cs
@@ -2,13 +2,16 @@
 using System.Configuration;
 using umbraco;
 using umbraco.editorControls;
+using umbraco.DataLayer;
+using System;
+using uComponents.DataTypes.Shared.PrevalueEditors;
 
 namespace uComponents.DataTypes.SqlDropDownList
 {
 	/// <summary>
 	/// The options for the SqlDropDownListOptions data-type.
 	/// </summary>
-	public class SqlDropDownListOptions : AbstractOptions
+	public class SqlDropDownListOptions : SqlOptions
 	{
 		/// <summary>
 		/// Initializes a new instance of the <see cref="SqlDropDownListOptions"/> class.
@@ -24,38 +27,6 @@ namespace uComponents.DataTypes.SqlDropDownList
 		public SqlDropDownListOptions(bool loadDefaults)
 			: base(loadDefaults)
 		{
-		}
-
-		/// <summary>
-		/// Sql expression used to get drop down list values
-		/// this expression must return both Text and Value fields
-		/// </summary>
-		[DefaultValue("")]
-		public string Sql { get; set; }
-
-		/// <summary>
-		/// Gets or sets an optional connection string (if null then umbraco connection string is used)
-		/// </summary>
-		[DefaultValue("")]
-		public string ConnectionStringName { get; set; }
-
-		/// <summary>
-		/// Checks web.config for a matching named connection string, else returns the current Umbraco database connection
-		/// </summary>
-		/// <returns>a connection string</returns>
-		public string GetConnectionString()
-		{
-			if (!string.IsNullOrWhiteSpace(this.ConnectionStringName))
-			{
-				// attempt to get connection string from the web.config
-				var connectionStringSettings = ConfigurationManager.ConnectionStrings[this.ConnectionStringName];
-				if (connectionStringSettings != null)
-				{
-					return connectionStringSettings.ConnectionString;
-				}
-			}
-
-			return uQuery.SqlHelper.ConnectionString; // default if unknown;
 		}
 	}
 }
