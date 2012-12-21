d16a17af18bd2370b39053ff482df17c9ba5259f
leekelleher <leekelleher@gmail.com>
2012-12-21 13:13:27 +0000

    Bumped the version number for v5.3.0
    --HG--
    branch : 5.3.0-dev

--- uComponents.Core/Properties/VersionInfo.cs
+++ uComponents.Core/Properties/VersionInfo.cs
@@ -14,8 +14,8 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("5.2.1")]
-[assembly: AssemblyFileVersion("5.2.889.24838")]
-[assembly: AssemblyInformationalVersion("5.2.1")]
+[assembly: AssemblyVersion("5.3.0")]
+[assembly: AssemblyFileVersion("5.3.906.35804")]
+[assembly: AssemblyInformationalVersion("5.3.0")]
 
 
