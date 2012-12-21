cae231ec11c069db870a2f39b2c217f94883f8a3
leekelleher <leekelleher@gmail.com>
2012-12-21 13:32:06 +0000

    SQL AutoComplete: Updated the API references for the RestExtension.
    --HG--
    branch : 6.0.0

--- uComponents.DataTypes/SqlAutoComplete/SqlAutoCompleteBase.cs
+++ uComponents.DataTypes/SqlAutoComplete/SqlAutoCompleteBase.cs
@@ -1,14 +1,11 @@
-﻿using Umbraco.Web.BaseRest;
-using System.Data;
+﻿using System.Collections.Generic;
 using System.Data.Common;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Script.Serialization;
 using System.Xml.Linq;
-using umbraco;
 using umbraco.DataLayer;
-using umbraco.presentation.umbracobase;
+using Umbraco.Web.BaseRest;
 
 namespace uComponents.DataTypes.SqlAutoComplete
 {
