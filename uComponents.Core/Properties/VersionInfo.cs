0e0ea150462ad0f7755b2129a1dc04dbb0d756fd
leekelleher <leekelleher@gmail.com>
2013-02-04 19:27:15 +0000

    Bumped version number. Closed 5.3.2-dev branch.
    --HG--
    branch : 5.3.2-dev
    extra : close : 1

--- uComponents.Core/Properties/VersionInfo.cs
+++ uComponents.Core/Properties/VersionInfo.cs
@@ -14,8 +14,8 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("5.3.1")]
-[assembly: AssemblyFileVersion("5.3.948.30981")]
-[assembly: AssemblyInformationalVersion("5.3.1")]
+[assembly: AssemblyVersion("5.3.2")]
+[assembly: AssemblyFileVersion("5.3.951.52017")]
+[assembly: AssemblyInformationalVersion("5.3.2")]
 
 
