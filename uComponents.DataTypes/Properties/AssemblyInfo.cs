143587bd9dc112663b949c60d12774d6f2f3d9cb
leekelleher <leekelleher@gmail.com>
2013-02-14 14:02:29 +0000

    Fixed [workitem:14806] Similarity - missing images and styles.
    --HG--
    branch : 5.4.0-dev
    extra : rebase_source : cb603842aa33fe6d409f2a446cc4e216b171e738

--- uComponents.DataTypes/Properties/AssemblyInfo.cs
+++ uComponents.DataTypes/Properties/AssemblyInfo.cs
@@ -8,11 +8,7 @@ using uComponents.Core;
 // shared embedded resources
 [assembly: WebResource(Constants.PrevalueEditorCssResourcePath, Constants.MediaTypeNames.Text.Css, PerformSubstitution = true)]
 [assembly: WebResource("uComponents.DataTypes.Shared.Resources.Scripts.jquery.form.js", Constants.MediaTypeNames.Application.JavaScript)]
-[assembly: WebResource("uComponents.DataTypes.Shared.Resources.Scripts.jquery.tooltip.min.js", Constants.MediaTypeNames.Application.JavaScript)]
 [assembly: WebResource("uComponents.DataTypes.Shared.Resources.Scripts.json2.js", Constants.MediaTypeNames.Application.JavaScript)]
 [assembly: WebResource("uComponents.DataTypes.Shared.Resources.Scripts.jquery.ucomponents.js", Constants.MediaTypeNames.Application.JavaScript)]
 [assembly: WebResource("uComponents.DataTypes.Shared.Resources.Scripts.jquery.ucomponents.dictionary.js", Constants.MediaTypeNames.Application.JavaScript)]
-[assembly: WebResource("uComponents.DataTypes.Shared.Resources.Images.deleteIcon.gif", Constants.MediaTypeNames.Image.Gif)]
-[assembly: WebResource("uComponents.DataTypes.Shared.Resources.Images.information.png", Constants.MediaTypeNames.Image.Png)]
-[assembly: WebResource("uComponents.DataTypes.Shared.Resources.Images.throbber.gif", Constants.MediaTypeNames.Image.Gif)]
 [assembly: WebResource("uComponents.DataTypes.Shared.Resources.Images.ucomponents-logo-small.png", Constants.MediaTypeNames.Image.Png)]
