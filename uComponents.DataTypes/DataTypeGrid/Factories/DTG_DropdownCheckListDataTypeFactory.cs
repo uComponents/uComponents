1b18546cac2a044061e4c0c86d4ce4c134f5c3c0
Ove Andersen <ove.andersen@outlook.com>
2013-02-27 16:45:36 +0100

    DTG: Added compatibility with the DropdownChecklist datatype
    --HG--
    branch : 6.0.0

---
+++ uComponents.DataTypes/DataTypeGrid/Factories/DTG_DropdownCheckListDataTypeFactory.cs
@@ -0,0 +1,31 @@
+﻿namespace uComponents.DataTypes.DataTypeGrid.Factories
+{
+    using System.Linq;
+
+    using uComponents.DataTypes.DataTypeGrid.Model;
+    using uComponents.DataTypes.DropdownCheckList;
+
+    using umbraco;
+    using umbraco.interfaces;
+
+    /// <summary>
+    /// Factory for the <see cref="DDCList_DataType"/>
+    /// </summary>
+    [DataTypeFactory(Priority = -1)]
+    public class DropdownCheckListDataTypeFactory : BaseDataTypeFactory<DDCList_DataType>
+    {
+        /// <summary>
+        /// Method for performing special actions <b>before</b> creating the <see cref="IDataType" /> editor.
+        /// </summary>
+        /// <param name="dataType">The <see cref="IDataType" /> instance.</param>
+        /// <param name="eventArgs">The <see cref="DataTypeLoadEventArgs"/> instance containing the event data.</param>
+        /// <remarks>Called <b>before</b> the grid creates the editor controls for the specified <see cref="IDataType" />.</remarks>
+        public override void Initialize(DDCList_DataType dataType, DataTypeLoadEventArgs eventArgs)
+        {
+            if (dataType.Data.Value == null)
+            {
+                dataType.Data.Value = string.Empty;
+            }
+        }
+    }
+}
