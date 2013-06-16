f0a82107d6b1a630ce261ed07916b4314f0ec54a
leekelleher <leekelleher@gmail.com>
2013-06-16 22:39:46 +0100

    Added csproj for Property Editor Convertors.
    --HG--
    branch : 6.0.0

---
+++ uComponents.PropertyEditors.Converters/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("uComponents.PropertyEditors.Converters")]
+[assembly: AssemblyDescription("Property Editor Converters for Umbraco/uComponents.")]
