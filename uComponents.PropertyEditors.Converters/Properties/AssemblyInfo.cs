c1ea5deccbafcaa470d6a2d73ae4f7bb7e3ded82
leekelleher <leekelleher@gmail.com>
2013-06-17 14:21:50 +0100

    Mistyped the folder name for the PropertyEditor Converters... ... it should have been PropertyEditorValueConverters!
    --HG--
    branch : 6.0.0
    rename : uComponents.PropertyEditors.Converters/Properties/AssemblyInfo.cs => uComponents.PropertyEditors.ValueConverters/Properties/AssemblyInfo.cs
    rename : uComponents.PropertyEditors.Converters/packages.config => uComponents.PropertyEditors.ValueConverters/packages.config
    rename : uComponents.PropertyEditors.Converters/uComponents.PropertyEditors.Converters.csproj => uComponents.PropertyEditors.ValueConverters/uComponents.PropertyEditors.ValueConverters.csproj
    extra : rebase_source : b0325a8ad367787b4455b301bdf5fe55f299b665

--- uComponents.PropertyEditors.Converters/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("uComponents.PropertyEditors.Converters")]
-[assembly: AssemblyDescription("Property Editor Converters for Umbraco/uComponents.")]
---
+++ uComponents.PropertyEditors.ValueConverters/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("uComponents.PropertyEditors.ValueConverters")]
+[assembly: AssemblyDescription("Property Editor Value Converters for Umbraco/uComponents.")]
