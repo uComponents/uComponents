54ff0c86404b4a812da0157e05a0844362d52a3c
leekelleher <leekelleher@gmail.com>
2013-06-19 11:07:35 +0100

    Added DTG PropertyEditorValueConverter
    --HG--
    branch : 6.0.0

---
+++ uComponents.PropertyEditors.ValueConverters/DataTypeGrid/DataTypeGridPropertyEditorValueConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using uComponents.DataTypes;
+using uComponents.DataTypes.DataTypeGrid.Model;
+using Umbraco.Core;
+using Umbraco.Core.PropertyEditors;
+using Umbraco.Web;
+
+namespace uComponents.PropertyEditors.ValueConverters.DataTypeGrid
+{
+	public class DataTypeGridPropertyEditorValueConverter : IPropertyEditorValueConverter
+	{
+		public bool IsConverterFor(Guid propertyEditorId, string docTypeAlias, string propertyTypeAlias)
+		{
+			return Guid.Parse(DataTypeConstants.DataTypeGridId).Equals(propertyEditorId);
+		}
+
+		public Attempt<object> ConvertPropertyValue(object value)
+		{
+			if (UmbracoContext.Current != null && value != null && value.ToString() != string.Empty)
+			{
+				try
+				{
+					return new Attempt<object>(true, new GridRowCollection(value.ToString()));
+				}
+				catch (Exception)
+				{
+					return Attempt<object>.False;
+				}
+			}
+
+			return Attempt<object>.False;
+		}
+	}
+}
