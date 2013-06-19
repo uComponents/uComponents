8d461f827f34226256cb74e1c8fe0186321e24b9
leekelleher <leekelleher@gmail.com>
2013-06-19 12:25:46 +0100

    Added PropertyEditorValueConverter for Similarity
    --HG--
    branch : 6.0.0
    extra : amend_source : 9b681f3c44caddb20858fa72ffa73380ee816fdb

---
+++ uComponents.PropertyEditors.ValueConverters/Similarity/SimilarityPropertyEditorValueConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using uComponents.DataTypes;
+using umbraco;
+using Umbraco.Core;
+using Umbraco.Core.PropertyEditors;
+using Umbraco.Web;
+
+namespace uComponents.PropertyEditors.ValueConverters.Similarity
+{
+	public class SimilarityPropertyEditorValueConverter : IPropertyEditorValueConverter
+	{
+		public bool IsConverterFor(Guid propertyEditorId, string docTypeAlias, string propertyTypeAlias)
+		{
+			return Guid.Parse(DataTypeConstants.SimilarityId).Equals(propertyEditorId);
+		}
+
+		public Attempt<object> ConvertPropertyValue(object value)
+		{
+			if (UmbracoContext.Current != null && value != null && value.ToString() != string.Empty)
+			{
+				try
+				{
+					var nodeIds = uQuery.GetCsvIds(value.ToString());
+					var helper = new UmbracoHelper(UmbracoContext.Current);
+					return new Attempt<object>(true, helper.TypedContent(nodeIds));
+				}
+				catch (Exception)
+				{
+					return Attempt<object>.False;
+				}
+			}
+
+			return Attempt<object>.False;
+		}
+	}
+}
