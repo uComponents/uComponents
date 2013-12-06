fa3a7f1b46ead038be1a7d3896850b69adcbb380
leekelleher <leekelleher@gmail.com>
2013-12-06 01:28:43 +0000

    Fixed the MSBuild script.
    Re-referenced the MSBuild tasks in the csproj.

--- build.cmd
+++ build.cmd
@@ -1,2 +1,8 @@
-C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /m /t:Package /p:"Configuration=Release";"MinUmbracoVersion=6.1.0";"PackageVersion=6.0.0-build";"BuildInParallel=true" BuildPackage.msbuild
-PAUSE
+:: Build the package zip
+C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe /m /t:Package /p:"Configuration=Release";"MinUmbracoVersion=6.1.0";"PackageVersion=6.0.1-build";"BuildInParallel=true" package\package.proj
+
+@IF %ERRORLEVEL% NEQ 0 GOTO err
+@EXIT /B 0
+:err
+@PAUSE
+@EXIT /B 1
