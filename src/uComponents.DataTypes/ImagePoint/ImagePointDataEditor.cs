f33a27d6cc463d239875812a47571e2536f0261e
Hendy Racher <git@hendyracher.co.uk>
2014-03-20 00:26:02 +0000

    ImagePoint - now renders neighbouring points as ghost markers

--- src/uComponents.DataTypes/ImagePoint/ImagePointDataEditor.cs
+++ src/uComponents.DataTypes/ImagePoint/ImagePointDataEditor.cs
@@ -19,11 +19,12 @@ using Image = System.Web.UI.WebControls.Image;
 [assembly: WebResource("uComponents.DataTypes.ImagePoint.ImagePoint.css", Constants.MediaTypeNames.Text.Css)]
 [assembly: WebResource("uComponents.DataTypes.ImagePoint.ImagePoint.js", Constants.MediaTypeNames.Application.JavaScript)]
 [assembly: WebResource("uComponents.DataTypes.ImagePoint.ImagePointMarker.png", Constants.MediaTypeNames.Image.Png)]
-
+[assembly: WebResource("uComponents.DataTypes.ImagePoint.ImagePointGhost.png", Constants.MediaTypeNames.Image.Png)]
 namespace uComponents.DataTypes.ImagePoint
 {
     using System.Xml.Linq;
     using umbraco.cms.businesslogic.property;
+    using umbraco.cms.businesslogic.media;
 
     /// <summary>
     /// Image Point Data Type
@@ -132,58 +133,6 @@ namespace uComponents.DataTypes.ImagePoint
         }
 
         /// <summary>
-        /// Called by the ASP.NET page framework to notify server controls that use composition-based implementation to create any child controls they contain in preparation for posting back or rendering.
-        /// </summary>
-        protected override void CreateChildControls()
-        {
-            /*  
-             * 
-             *  <div class="image-point">
-             *      X <input type="text" class="x" />
-             *      Y <input type="text" class="y" />
-             *      <div class="area">
-             *          <img src="" width="" height="" class="main" />
-             *          <img src="" class="marker" />
-             * 
-             *          (optional)
-             *          <img src="" class="ghost" data-x="" data-y="" />
-             *          <img src="" class="ghost" />
-             *          ...
-             * 
-             *      </div>
-             *  </div>
-             * 
-             */
-            this.div.Attributes.Add("class", "image-point");
-
-            this.xTextBox.ID = "xTextBox";
-            this.xTextBox.CssClass = "x";
-            this.xTextBox.MaxLength = 4;                        
-
-            this.yTextBox.ID = "yTextBox";
-            this.yTextBox.CssClass = "y";
-            this.yTextBox.MaxLength = 4;
-
-            WebControl areaDiv = new WebControl(HtmlTextWriterTag.Div);
-            areaDiv.CssClass = "area";
-            areaDiv.Controls.Add(this.mainImage);
-            areaDiv.Controls.Add(this.markerImage);
-
-            this.mainImage.CssClass = "main";           
-
-            this.markerImage.CssClass = "marker";
-            this.markerImage.ImageUrl = this.Page.ClientScript.GetWebResourceUrl(this.GetType(), "uComponents.DataTypes.ImagePoint.ImagePointMarker.png");
-
-            this.div.Controls.Add(new Literal() { Text = "X " });
-            this.div.Controls.Add(this.xTextBox);
-            this.div.Controls.Add(new Literal() { Text = " Y " });
-            this.div.Controls.Add(this.yTextBox);
-            this.div.Controls.Add(areaDiv);
-
-            this.Controls.Add(this.div);
-        }
-
-        /// <summary>
         /// Raises the <see cref="E:System.Web.UI.Control.Load"/> event.
         /// </summary>
         /// <param name="e">The <see cref="T:System.EventArgs"/> object that contains the event data.</param>
@@ -224,6 +173,64 @@ namespace uComponents.DataTypes.ImagePoint
         }
 
         /// <summary>
+        /// Called by the ASP.NET page framework to notify server controls that use composition-based implementation to create any child controls they contain in preparation for posting back or rendering.
+        /// </summary>
+        protected override void CreateChildControls()
+        {
+            /*  
+             * 
+             *  <div class="image-point">
+             *      X <input type="text" class="x" />
+             *      Y <input type="text" class="y" />
+             *      <div class="area">
+             *          <img src="" width="" height="" class="main" />
+             *          <img src="" class="marker" />
+             * 
+             *          (optional)
+             *          <img src="" class="ghost" style="left:x;top:y;" />
+             *          <img src="" class="ghost" style="left:x;top:y;" />
+             *          <img src="" class="ghost" style="left:x;top:y;" />
+             *          ...
+             * 
+             *      </div>
+             *  </div>
+             * 
+             */
+            this.div.Attributes.Add("class", "image-point");
+
+            this.xTextBox.ID = "xTextBox";
+            this.xTextBox.CssClass = "x";
+            this.xTextBox.MaxLength = 4;
+
+            this.yTextBox.ID = "yTextBox";
+            this.yTextBox.CssClass = "y";
+            this.yTextBox.MaxLength = 4;
+
+            this.mainImage.CssClass = "main";
+
+            this.markerImage.CssClass = "marker";
+            this.markerImage.ImageUrl = this.Page.ClientScript.GetWebResourceUrl(this.GetType(), "uComponents.DataTypes.ImagePoint.ImagePointMarker.png");
+
+            WebControl areaDiv = new WebControl(HtmlTextWriterTag.Div) { CssClass = "area" };
+
+            areaDiv.Controls.Add(this.mainImage);
+            areaDiv.Controls.Add(this.markerImage);
+
+            foreach(Image ghostImage in this.ghostImages.Where(x => x != null)) // single use of collection, so single place for null check
+            {
+                areaDiv.Controls.Add(ghostImage);
+            }
+
+            this.div.Controls.Add(new Literal() { Text = "X " });
+            this.div.Controls.Add(this.xTextBox);
+            this.div.Controls.Add(new Literal() { Text = " Y " });
+            this.div.Controls.Add(this.yTextBox);
+            this.div.Controls.Add(areaDiv);
+
+            this.Controls.Add(this.div);
+        }
+
+        /// <summary>
         /// Called by Umbraco when saving the node
         /// </summary>
         public void Save()
@@ -244,81 +251,93 @@ namespace uComponents.DataTypes.ImagePoint
         /// if a width or height value has been supplied, and one of these = 0, then it needs to be calculated from the image dimensions (so as to keep the aspect ratio)
         /// </summary>
         private void InitializeControls()
-        {            
+        {
             int width = 0; // default unknown values
             int height = 0;
             Size imageSize = new Size(0, 0);
-           
+
             if (!string.IsNullOrWhiteSpace(this.options.ImagePropertyAlias))
             {
                 string imageUrl = null;
+                int currentDataTypeDefinitionId = ((XmlData)this.data).DataTypeDefinitionId;
+                int currentPropertyId = ((XmlData)this.data).PropertyId;
 
-                // used large try/catch block to simply checking for nulls on content / media & members
-                try
+                // determine whether to use the Document / Media or Member api
+                switch (uQuery.GetUmbracoObjectType(this.CurrentContentId))
                 {
-                    // looking for the specified property
-                    switch (uQuery.GetUmbracoObjectType(this.CurrentContentId))
-                    {
-                        case uQuery.UmbracoObjectType.Document:
+                    case uQuery.UmbracoObjectType.Document:
 
-                            Document imageDocument = uQuery.GetDocument(this.CurrentContentId)
-                                                            .GetAncestorOrSelfDocuments()
-                                                            .First(x => x.HasProperty(this.options.ImagePropertyAlias));
-                            
+                        Document imageDocument = uQuery.GetDocument(this.CurrentContentId)
+                                                        .GetAncestorOrSelfDocuments()
+                                                        .FirstOrDefault(x => x.HasProperty(this.options.ImagePropertyAlias));
+
+                        if (imageDocument != null)
+                        {
                             imageUrl = imageDocument.GetProperty<string>(this.options.ImagePropertyAlias);
 
-                            // find all self or descendant documents that have this specifc datatype datatype, but stop if any contain the imagePropertyAlias
                             if (this.options.ShowNeighbours)
                             {
-                                List<Document> documents = new List<Document>(); // building a collection as there isn't a .GetDescendantsOrSelf(Func<Document, bool> func) overload
-                                documents.Add(imageDocument); // add the top level document
-                                documents.AddRange(imageDocument.GetDescendantDocuments(x => !x.HasProperty(this.options.ImagePropertyAlias))); // drill down and get all documents, but stop descending if another the imagePropertyAlias is found
+                                // get a collection of all documents that could potentially have this imagepoint datatype instance, sharing the same image
+                                List<Document> documents = new List<Document>();  
+                                
+                                // add the document containing the shared main image
+                                documents.Add(imageDocument); 
 
-                                Image ghostImage;
-                                ImagePoint imagePoint;
+                                // drill down and get all documents, but stop descending a path if an imagePropertyAlias is found - any descendants of that would use a different image
+                                documents.AddRange(imageDocument.GetDescendantDocuments(x => !x.HasProperty(this.options.ImagePropertyAlias))); 
 
-                                // for all documents that contain this instance of this datatype
-                                foreach (Document document in documents.Where(x => x.GenericProperties.Select(y => y.PropertyType.DataTypeDefinition.Id).Contains(((XmlData)this.data).DataTypeDefinitionId)))
+                                // for all documents that use this instance of this imagepoint datatype
+                                foreach (Document document in documents.Where(x => x.GenericProperties.Select(y => y.PropertyType.DataTypeDefinition.Id).Contains(currentDataTypeDefinitionId)))
                                 {
-                                    // the document may contain multiple instance of the ImagePoint datatype, but with different aliases
-                                    foreach (Property property in document.GenericProperties.Where(x => x.PropertyType.DataTypeDefinition.Id == ((XmlData)this.data).DataTypeDefinitionId))
+                                    // the document may have multiple properties each using this imagepoint datatype instance
+                                    foreach (Property property in document.GenericProperties.Where(x => x.PropertyType.DataTypeDefinition.Id == currentDataTypeDefinitionId && x.Id != currentPropertyId))
                                     {
-                                        imagePoint = new ImagePoint();
-                                        ((uQuery.IGetProperty)imagePoint).LoadPropertyValue(property.Value.ToString());
-
-                                        if (imagePoint.HasCoordinate)
-                                        {
-                                            ghostImage = new Image();
-                                            ghostImage.Attributes.Add("data-x", imagePoint.X.ToString());
-                                            ghostImage.Attributes.Add("data-y", imagePoint.Y.ToString());
-
-                                            this.ghostImages.Add(ghostImage);
-                                        }
+                                        this.ghostImages.Add(this.GetGhostImage(property));
                                     }
                                 }
-                            }                                            
+                            }
+                        }
 
-                            break;
+                        break;
 
-                        case uQuery.UmbracoObjectType.Media:
+                    case uQuery.UmbracoObjectType.Media:
 
-                            imageUrl = uQuery.GetMedia(this.CurrentContentId)
-                                                .GetAncestorOrSelfMedia()
-                                                .First(x => x.HasProperty(this.options.ImagePropertyAlias))
-                                                .GetProperty<string>(this.options.ImagePropertyAlias);
-                            break;
+                        Media imageMedia = uQuery.GetMedia(this.CurrentContentId)
+                                                    .GetAncestorOrSelfMedia()
+                                                    .FirstOrDefault(x => x.HasProperty(this.options.ImagePropertyAlias));
+                        if (imageMedia != null)
+                        {
+                            imageUrl = imageMedia.GetProperty<string>(this.options.ImagePropertyAlias);
 
-                        case uQuery.UmbracoObjectType.Member:
+                            if (this.options.ShowNeighbours)
+                            {
+                                List<Media> media = new List<Media>();
 
-                            imageUrl = uQuery.GetMember(this.CurrentContentId).GetProperty<string>(this.options.ImagePropertyAlias);
+                                // add the media item containing the shared main image
+                                media.Add(imageMedia);
 
-                            break;
-                    }
-                }
-                catch
-                {
-                    // node, media or member with specified property couldn't be found
-                    // TODO: if debug mode on, then thow exception, else be silent
+                                // unfortunately no uQuery extension method, so using a private local
+                                media.AddRange(this.GetDescendantMedia(imageMedia, x => !x.HasProperty(this.options.ImagePropertyAlias)));
+
+                                foreach(Media mediaItem in media.Where(x => x.GenericProperties.Select(y => y.PropertyType.DataTypeDefinition.Id).Contains(currentDataTypeDefinitionId)))
+                                {
+                                    foreach (Property property in mediaItem.GenericProperties.Where(x => x.PropertyType.DataTypeDefinition.Id == currentDataTypeDefinitionId && x.Id != currentPropertyId))
+                                    {
+                                        this.ghostImages.Add(this.GetGhostImage(property));
+                                    }
+                                }
+                            }
+                        }
+
+                        break;
+
+                    case uQuery.UmbracoObjectType.Member:
+
+                        imageUrl = uQuery.GetMember(this.CurrentContentId).GetProperty<string>(this.options.ImagePropertyAlias);
+
+                        // show neighbours not relevant
+
+                        break;
                 }
 
                 if (!string.IsNullOrWhiteSpace(imageUrl))
@@ -326,7 +345,7 @@ namespace uComponents.DataTypes.ImagePoint
                     // attempt to get image from the url
                     string imagePath = HttpContext.Current.Server.MapPath(imageUrl);
                     if (File.Exists(imagePath))
-                    {                        
+                    {
                         using (System.Drawing.Image image = System.Drawing.Image.FromFile(imagePath))
                         {
                             this.mainImage.ImageUrl = imageUrl;
@@ -336,13 +355,13 @@ namespace uComponents.DataTypes.ImagePoint
                     }
                 }
             }
-            
+
             if (this.options.Width == 0 && this.options.Height == 0)
             {
                 // neither value set, so use image dimensions
                 width = imageSize.Width;
                 height = imageSize.Height;
-            }            
+            }
             else if (this.options.Width > 0 && this.options.Height == 0)
             {
                 // width set, so calulate height
@@ -352,7 +371,7 @@ namespace uComponents.DataTypes.ImagePoint
                 {
                     height = (int)(imageSize.Height / ((decimal)(imageSize.Width / this.options.Width)));
                 }
-            }            
+            }
             else if (this.options.Width == 0 && this.options.Height > 0)
             {
                 // height set, so calculate width
@@ -375,6 +394,54 @@ namespace uComponents.DataTypes.ImagePoint
 
             //this.mainImage.Height = height;
             this.mainImage.Attributes["height"] = height.ToString();
+        }
+
+        /// <summary>
+        /// From a document or media property, attempt to get the imagepoint coordinate to use as a ghost image
+        /// </summary>
+        /// <param name="property"></param>
+        /// <returns>an Image or null</returns>
+        private Image GetGhostImage(Property property)
+        {
+            ImagePoint imagePoint = new ImagePoint();
+            ((uQuery.IGetProperty)imagePoint).LoadPropertyValue(property.Value.ToString());
+
+            if (imagePoint.HasCoordinate)
+            {
+                Image ghostImage = new Image();
+
+                ghostImage.ImageUrl = this.Page.ClientScript.GetWebResourceUrl(this.GetType(), "uComponents.DataTypes.ImagePoint.ImagePointGhost.png");
+                ghostImage.CssClass = "ghost";                
+
+                ghostImage.Style.Add(HtmlTextWriterStyle.Left, (imagePoint.X - 7).ToString() + "px");
+                ghostImage.Style.Add(HtmlTextWriterStyle.Top, (imagePoint.Y - 7).ToString() + "px");
+
+                return ghostImage;
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// No idea where to put this method !
+        /// </summary>
+        /// <param name="media"></param>
+        /// <param name="func"></param>
+        /// <returns></returns>
+        private IEnumerable<Media> GetDescendantMedia(Media media, Func<Media, bool> func)
+        {
+            foreach (Media childMedia in media.Children)
+            {
+                if (func(childMedia))
+                {
+                    yield return childMedia;
+
+                    foreach (Media descendantMedia in this.GetDescendantMedia(childMedia, func))
+                    {
+                        yield return descendantMedia;
+                    }
+                }
+            }
         }
     }
 }
