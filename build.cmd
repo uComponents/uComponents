1d7e31681230361f6a9398cbb8788cca78a8cc67
leekelleher <leekelleher@gmail.com>
2014-09-17 16:37:46 +0100

    Bumped version number

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 :: Build the package zip
-C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe /m /t:Package /p:"Configuration=Release";"MinUmbracoVersion=6.1.0";"PackageVersion=6.0.1-build";"BuildInParallel=true" package\package.proj
+C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\msbuild.exe /m /t:Package /p:"Configuration=Release";"MinUmbracoVersion=6.1.0";"PackageVersion=6.1.0-build";"BuildInParallel=true" package\package.proj
 
 @IF %ERRORLEVEL% NEQ 0 GOTO err
 @EXIT /B 0
